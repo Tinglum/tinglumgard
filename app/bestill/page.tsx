3f4b0264a92f9b8ed745cfb35a979e0d6dd0a656
Tinglum <5366384@gmail.com>
2026-01-29 20:59:03 +0100

    Add Vipps logo to security message on checkout page

--- app/bestill/page.tsx
+++ app/bestill/page.tsx
@@ -832,9 +832,7 @@ export default function CheckoutPage() {
                   {/* Info text */}
                   <div className="pt-4 border-t-2 border-gray-200">
                     <div className="flex items-center justify-center gap-2 text-sm text-gray-600 mb-2">
-                      <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                        <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
-                      </svg>
+                      <img src="/vipps-logo.svg" alt="Vipps" className="w-6 h-6" />
                       <span className="font-medium">Sikker betaling med Vipps</span>
                     </div>
                     <p className="text-xs text-center text-gray-500">
